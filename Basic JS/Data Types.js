57080f146f351ad655ee538ec6e9ed3f43bab2a0
Zia Ul Hassan Chowdhury <Ziaulhassanchowdhury18@gmail.com>
2024-11-28 13:09:20 +0600

    datatypes

--- Basic JS/Data Types.js
+++ Basic JS/Data Types.js
@@ -1,1 +1,85 @@
-console.log("zia")
+/*  seven primitive data types:
+Strings
+Number
+Boolean
+null
+undefined
+BigInt
+Symbol
+
+The Object data type contains the 3 sub-data types:
+Object
+Array
+Date
+*/
+
+let a = 500;
+let b = 499; //number
+let add = a+b;
+console.log(add)
+
+ let sub = a-b;
+ console.log(sub) 
+ 
+ let multi = a*b;
+ console.log(multi)
+
+ let test = a**b; //a^b
+ console.log(test)
+
+
+ let str1 = "Hello World"
+ let str2 = "Hello People" //string
+ console.log(str1)
+ console.log(str2)
+
+
+
+ /*JavaScript Symbol:
+ It is used to create unique primitive, and immutable values.
+ The Symbol() constructor can be used to create a unique symbol, and you may pass the string as a parameter of the Symbol() constructor. */
+/*
+ const x = symbol("Java");
+ console.log(x.description)*/
+
+
+//JavaScript Object: object data type allows us to store the collection of the data in the key-value format.
+let car = {
+    BrandName: "BMW",
+    Color: "Black",
+    Tire: 4,
+}
+for(let key in car) {
+    console.log(key + ": " + car[key]);
+}
+
+
+let animal = {
+    Tiger: "Panthera tigris",
+    Lion: "Panthera leo",
+    Deer: "Cervidae",
+}
+for (let key in animal) {
+    console.log(key + ": " + animal[key]);
+}
+
+
+//JavaScript Array: array is a list of elements of the different data types. You can create an array using two square brackets '[]' and insert multiple comma seprated values inside the array.
+
+const arr = ["Clementine", "Date Plums", "Dates", "Orange", "Banana", "Apple"];
+document.write("Winter Fruits is"+ ": " + arr);
+
+//Using for loop
+let Fruits = ["Clementine", "Date Plums", "Dates", "Orange", "Banana", "Apple"];
+for(let i=0; i<Fruits.length; i++) //Fruits.lenth: it automatically calculate the arry length
+    { 
+    console.log("Helthy Fruits"+ ": " + Fruits[i]); 
+    console.log(i+ ": "+arr[i]); //print arr value with index
+}
+
+let number = [500, 100, 200, 300, 800, 900, 1000]
+for (let i=0; i<10; i++)//i<10: in arry index less than 10 print, than stop
+    {
+    console.log(number[i])
+}
+
--- Basic JS/Variable.js
+++ Basic JS/Variable.js
@@ -1,12 +1,34 @@
-//var,let,const
+/*variables are used to store data.
+JavaScript, you can declare the variables in 4 ways −
+Using the 'var' keyword.
+Using the 'let' keyword.
+Using the 'const' keyword.
+*/
 
 
-var x = 10; //var can use many time
-let y = 20;
-const z = 970;
+var x = 10; //Re-declare & Re-update possible
+let y = 20; //Re-declare not possivle but Re-update possible
+const z = 970; //Re-declare and Re-update not possible
 console.log("const: " +z)
 console.log("let: " +y)
 console.log("var: " +x)
 
+/*the variable can hold the value of the dynamic data type. For example, you can store the value of number(5), string("java"), boolean(true/false), object, etc. data type values in JavaScript variables.
+var num = 50; //Number
+var str = "JavaScript" //String
+var bool = True; //Bollean
+*/
 
 
+/*Undefined Variable Value in JavaScript:
+When you don't initialize the variable after declaring, it contains the undefined value*/
+var num;
+      document.write("The value of num is: " + num + "<br/>"); //The value of num is undefined
+
+/* Global Variables − A global variable has global scope which means it can be defined anywhere in your JavaScript code.
+Local Variables − A local variable will be visible only within a function where it is defined. Function parameters are always local to that function. */
+
+
+
+
+ 
